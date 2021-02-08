e8711b74ca46da9c624d9fb8af0ce11b73a04bfa
RusyaZamorin <59511793+RusyaZamorin@users.noreply.github.com>
2021-02-08 14:59:30 +0200

    [+]

---
+++ Assets/Scripts/Application/GameObjectEntityImplementations/CharacterCardObjects/CardEditableButton/ActionOnCharacteristic.cs
@@ -0,0 +1,22 @@
+﻿using Application.CoreEntities;
+using CharacteristicsCalculator;
+
+namespace Application.GameObjectEntityImplementations
+{
+    public class ActionOnCharacteristic
+    {
+        public string CharacteristicName;
+        public Function Function;
+
+        public ActionOnCharacteristic(string characteristicName)
+        {
+            CharacteristicName = characteristicName;
+        }
+        
+        public void SetCharacteristicValue(Character character, double value)
+        {
+            character.SetCharacteristicValue(CharacteristicName, Function.Calculate(value, character));
+        }
+    }
+}
+
---
+++ Assets/Scripts/Application/GameObjectEntityImplementations/CharacterCardObjects/CardEditableButton/EditableButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CharacteristicsCalculator;
+
+namespace Application.GameObjectEntityImplementations
+{
+    public class EditableButton : MonoBehaviour
+    {
+        private Function _function;
+
+        public void Init(Function function)
+        {
+
+        }
+
+        public void SetFunction(Function function) => _function = function;
+    }
+}
+
